6e6b9351be567fd24528d0c2a500efa167d81848
Jonathan Neal <jonathantneal@hotmail.com>
2017-03-01 12:31:56 -0500

    Use walkDecls without walkRules

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ module.exports = postcss.plugin('postcss-replace-overflow-wrap', function (opts)
     var method = opts.method || 'replace';
 
     return function (css, result) { // eslint-disable-line no-unused-vars
-        css.walkRules(function (rule) {
-            rule.walkDecls(function (decl, i) { // eslint-disable-line no-unused-vars
-                if (decl.prop === 'overflow-wrap') {
-                    decl.cloneBefore({ prop: 'word-wrap' });
-                    if (method === 'replace') {
-                        decl.remove();
-                    }
+        css.walkDecls(function (decl, i) { // eslint-disable-line no-unused-vars
+            if (decl.prop === 'overflow-wrap') {
+                decl.cloneBefore({ prop: 'word-wrap' });
+                if (method === 'replace') {
+                    decl.remove();
                 }
-            });
+            }
         });
     };
 });
