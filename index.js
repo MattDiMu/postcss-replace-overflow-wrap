0e415117ddcec0577318fa4e959ba243968a4452
Jonathan Neal <jonathantneal@hotmail.com>
2017-05-10 19:28:23 -0400

    Remove unused vars

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = postcss.plugin('postcss-replace-overflow-wrap', (opts) => {
     opts = opts || {};
     const method = opts.method || 'replace';
 
-    return (css, result) => { // eslint-disable-line no-unused-vars
-        css.walkDecls('overflow-wrap', (decl, i) => { // eslint-disable-line no-unused-vars
+    return (css) => {
+        css.walkDecls('overflow-wrap', (decl) => {
             decl.cloneBefore({ prop: 'word-wrap' });
             if (method === 'replace') {
                 decl.remove();
